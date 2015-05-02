301f49a2ecda9c2f1b2c8e55080f3280dc52a22f
Victor Olteanu <volteanu@cisco.com>
2015-05-01 18:40:52 -0700

    initial ember app structure setup. added location input

---
+++ app/controllers/application.js
@@ -0,0 +1,10 @@
+export default Ember.Controller.extend({
+  myLocation: '94040',
+  periodFrom: '',
+  periodTo: '',
+  actions: {
+    updateData: function() {
+      //update info 
+    }
+  }
+});
